cc2588cef383db9cb78ff4aa2bab7d9a45d632c9
monayemislam <monayemislam@gmail.com>
2023-04-06 00:12:57 +0600

    Get surah by id

---
+++ src/components/SurahQuran.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="surahResult">
+        {{ surahResult }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+        name: 'SurahQuran',
+        data(){
+            return {
+                surahURL: 'http://api.alquran.cloud/v1/surah/114/en.asad',
+                surahResult: null
+            }
+        },
+        methods: {
+            getSurahByID : function(){
+                axios.get(this.surahURL)
+                .then((response)=>(this.surahResult=response.data))
+                .catch((error)=>{
+                    console.error(error);
+                });
+            }
+        },
+        mounted(){
+            this.getSurahByID();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import {createRouter, createWebHistory} from 'vue-router'
 import FullQuranAsad from '@/components/FullQuranAsad'
 import FullQuranUthmani from '@/components/FullQuranUthmani'
 import FullQuranAudio from '@/components/FullQuranAudio'
+import SurahQuran from '@/components/SurahQuran'
 
 
 const routes = [
   { path: '/full-quran-asad', name: "FullQuranAsad", component: FullQuranAsad },
   { path: '/full-quran-uthmani', name: "FullQuranUthmani", component: FullQuranUthmani },
   { path: '/full-quran-audio', name: "FullQuranAudio", component: FullQuranAudio },
+  { path: '/surah', name: "SurahQuran", component: SurahQuran },
 ]
 
 const router = createRouter({
